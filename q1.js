999737e495a37e376d1d08a2670949011c5a5ae2
Lorel <lorel.oboue@gmail.com>
2022-06-06 14:51:20 +0100

    Question 1 complete

---
+++ q1.js
@@ -0,0 +1,12 @@
+// Description of what I believe the functions are doing:
+// I understand that myFunction is a function that has two arguments being num1 and num2 (num1, num2). Inside the body of the function there is an instruction to ADD (demonstared with the + operator) the two arguments and RETURN the answer.
+//const result is a variable that calls myFunction with num1 and num2, which in this case is 5 and 5. it will RETURN 10 and the result (the variable).
+
+
+function myFunction(num1, num2) {
+    return num1 + num2
+}
+
+const result = console.log(myFunction(5,5))
+
+// After running the code, I was CORRECT
